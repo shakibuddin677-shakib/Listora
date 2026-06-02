cdf3c19d56b47446607c1ed2965a6719deeaf42f
Shakib Uddin <shakibuddin677@gmail.com>
2026-06-02 16:28:34 +0530

    add cloudinary test route

--- cloudConfig.js
+++ cloudConfig.js
@@ -4,14 +4,13 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  api_secret: process.env.CLOUD_API_SECRET?.trim(),
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
     folder: "wanderlust_Dev",
-    allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
 
